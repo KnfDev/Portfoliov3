fc22c64de8bb849462f14a0f68b571a6fb35320a
Kenneth <iamknf@gmail.com>
2022-10-19 17:51:45 -1000

    work on header component

--- frontend/src/containers/Header/Header.js
+++ frontend/src/containers/Header/Header.js
@@ -1,10 +1,75 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import { motion } from "framer-motion";
+import "./Header.scss";
+import { images } from "../../utils";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+const skills = [images.javascript, images.react];
+const skillList = skills.map((skill, index) => {
+  return (
+    <div className="circle-cmp app__flex" key={`circle-${index}`}>
+      <img src={skill} alt="skill-icon" />
+    </div>
+  );
+});
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        className="app__header-info"
+        whileInView={{ x: [-150, 0], opacity: [0, 1] }}
+        transition={{ duration: 1.5 }}
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Aloha World!</p>
+              <h1 className="head-text" alt="header text">
+                I'm Kenneth
+              </h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Software Developer</p>
+            <p className="p-text">Full-Stack</p>
+          </div>
+        </div>
+      </motion.div>
 
-export default Header
+      <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header-img"
+    >
+      <img className='profile' src={images.photo} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.photo}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
+
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {skillList}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/utils/images.js
@@ -0,0 +1,13 @@
+import photo from '../images/Photo.png'
+import photo2 from '../images/Photo-1.png'
+import circle from '../images/circle.svg'
+import javascript from '../images/javascript-original.svg'
+import react from '../images/react-original.svg'
+import bgIMG from '../images/bgIMG.png'
+export default {
+  photo,
+  photo2,
+  circle,
+  javascript,
+  react
+}
